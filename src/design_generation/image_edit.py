385057bf5a01abdb6d4f12a4a0359ee500332036
Samir <shc220@ic.ac.uk>
2025-01-29 23:00:31 +0000

    feat: provide utils for removing bg and cropping

---
+++ src/design_generation/image_edit.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import rembg
+
+
+def remove_bg(image: Image.Image) -> Image.Image:
+    return rembg.remove(image)
+
+
+def crop_to_content(image: Image.Image) -> Image.Image:
+    """
+    Crops an image with transparency to the non-transparent content area.
+
+    :param image: Input image with transparency
+
+    :return out: Image cropped to content
+    """
+
+    bbox = image.getbbox()
+    if bbox:
+        return image.crop(bbox)
+    else:
+        return Image.new("RGBA", (1, 1), (0, 0, 0, 0))
+
+
+if __name__ == "__main__":
+    from src.ml.image_gen import StableDiffusion1_5_Txt2ImgModel
+
+    image_model = StableDiffusion1_5_Txt2ImgModel()
+    image = image_model.generate_image(
+        prompt="Minimalist T-shirt design of planet Earth, modern aesthetic",
+        negative_prompt="blurry, messy, over-detailed, extra elements, complex, photorealistic",
+        height=768,
+        width=768,
+        num_inference_steps=50,
+        num_images_per_prompt=1
+    )
+    image.show()
+    image = remove_bg(image)
+    image = crop_to_content(image)
+    image.show()
