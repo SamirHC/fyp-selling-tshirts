654abddd689062d4371dd9be71e2860bc270824a
Samir <shc220@ic.ac.uk>
2025-01-29 22:21:12 +0000

    feat: add stable diffusion 1.5 model

--- src/ml/image_gen.py
+++ src/ml/image_gen.py
@@ -22,6 +22,18 @@ class DummyImageModel(ImageModel):
         return Image.new("RGBA", (100, 100), "red")
 
 
+class StableDiffusion1_5_Txt2ImgModel(ImageModel):
+    def __init__(self):
+        MODEL = "sd-legacy/stable-diffusion-v1-5"
+        self.pipe = StableDiffusionPipeline.from_pretrained(MODEL)
+        self.pipe = self.pipe.to("cuda")
+
+    def generate_image(self, **kwargs):
+        pipe = self.pipe(**kwargs)
+        image = pipe.images[0]
+        return image
+
+
 class StableDiffusion2_1_Txt2ImgModel(ImageModel):
     def __init__(self):
         MODEL = "stabilityai/stable-diffusion-2-1"
@@ -35,7 +47,7 @@ class StableDiffusion2_1_Txt2ImgModel(ImageModel):
 
 
 if __name__ == "__main__":
-    image_model = StableDiffusion2_1_Txt2ImgModel()
+    image_model = StableDiffusion1_5_Txt2ImgModel()
     image = image_model.generate_image(
         prompt="Minimalist T-shirt design of planet Earth, modern aesthetic",
         negative_prompt="blurry, messy, over-detailed, extra elements, complex, photorealistic",
