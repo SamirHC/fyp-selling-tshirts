20f5d38dd2c28945f5bb418bb3903aa41ac851b4
Samir <shc220@ic.ac.uk>
2025-01-30 14:26:47 +0000

    feat: add inpainting model for improved generation for T-shirt context

--- src/ml/image_gen.py
+++ src/ml/image_gen.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionInpaintPipeline
 from dotenv import dotenv_values
 from huggingface_hub import login
 from PIL import Image
+import torch
 
 
 config = dotenv_values(".env")
@@ -46,7 +47,20 @@ class StableDiffusion2_1_Txt2ImgModel(ImageModel):
         return image
 
 
+class StableDiffusionInpaintingImageModel(ImageModel):
+    def __init__(self):
+        MODEL = "sd-legacy/stable-diffusion-inpainting"
+        self.pipe = StableDiffusionInpaintPipeline.from_pretrained(MODEL, torch_dtype=torch.float16)
+        self.pipe = self.pipe.to("cuda")
+
+    def generate_image(self, **kwargs):
+        pipe = self.pipe(**kwargs)
+        image = pipe.images[0]
+        return image
+
+
 if __name__ == "__main__":
+    """
     image_model = StableDiffusion1_5_Txt2ImgModel()
     image = image_model.generate_image(
         prompt="Minimalist T-shirt design of planet Earth, modern aesthetic",
@@ -55,5 +69,21 @@ if __name__ == "__main__":
         width=768,
         num_inference_steps=50,
         num_images_per_prompt=1
+    )
+    """
+    import os
+
+    empty_tshirt_image_path = os.path.join("data", "images", "EmptyTshirt2.png")
+    empty_tshirt_image = Image.open(empty_tshirt_image_path).resize((512, 512), Image.Resampling.LANCZOS).convert("RGB")
+    mask_path = os.path.join("data", "images", "EmptyTshirt2Mask.png")
+    mask = Image.open(mask_path).resize((512, 512), Image.Resampling.LANCZOS).convert("1")
+
+    image_model = StableDiffusionInpaintingImageModel()
+    image = image_model.generate_image(
+        prompt="Minimalist planet Earth T-shirt, modern aesthetic",
+        negative_prompt="blurry, messy, over-detailed, extra elements, complex, photorealistic",
+        image=empty_tshirt_image,
+        mask_image=mask,
+        strength=0.95,
     )
     image.show()
