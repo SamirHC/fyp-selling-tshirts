04be551ec0abe5c3e4cb0ea055465edf59c691a3
Samir <shc220@ic.ac.uk>
2025-02-23 04:46:29 +0000

    feat: 3d colour plot for demonstrating kmeans palette search

--- src/ml/color_theme_classifier.py
+++ src/ml/color_theme_classifier.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from PIL import Image
 
 from src.common import utils
 from src.data_collection import palettes
@@ -12,7 +13,7 @@ class ColorThemeClassifier:
     color_df = palettes.get_palette_data()
 
     @staticmethod
-    def extract_colors(image, seed=23920):
+    def extract_colors(image: Image.Image, seed=23920):
         pixels = np.array(image).reshape(-1, 3)
 
         num_colors = 4
@@ -46,7 +47,7 @@ class ColorThemeClassifier:
         return row_idx, nearest_colors, min_dist
 
     @staticmethod
-    def get_palette_data(image) -> pd.Series:
+    def get_palette_data(image: Image.Image) -> pd.Series:
         colors_rgb = ColorThemeClassifier.extract_colors(image)
         row_idx, nearest_colors, dist = ColorThemeClassifier.find_closest_palette(colors_rgb)
         
@@ -64,7 +65,7 @@ class ColorThemeClassifier:
         })
 
     @staticmethod
-    def show_data(image):
+    def show_data(image: Image.Image):
         palette_data = ColorThemeClassifier.get_palette_data(image)
         print(palette_data)
 
@@ -83,9 +84,43 @@ class ColorThemeClassifier:
         axs[2].imshow(image)
 
         plt.show()
+    
+    @staticmethod
+    def plot_pixel_colors(image: Image.Image, n=2000) -> plt.Figure:
+        colors = np.array(image).reshape(-1, 3)
+        
+        # Uniform sampling of the colours to reduce number of pixels plotted
+        indices = np.arange(len(colors))
+        np.random.shuffle(indices)
+        indices = indices[:n]
+
+        colors = colors[indices, :]
+
+        r = colors[:, 0]
+        g = colors[:, 1]
+        b = colors[:, 2]
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+        ax.set_xlabel("Red")
+        ax.set_ylabel("Blue")
+        ax.set_zlabel("Green")
+
+        ax.set_xlim(0, 255)
+        ax.set_ylim(0, 255)
+        ax.set_zlim(0, 255)
+
+        ax.scatter(r, g, b, c=colors/255)
+
+        return fig
+
 
 if __name__ == "__main__":
-    df = utils.load_data("data/dataframes/seller_hub_data/ebay_data.pickle")
-    image = utils.get_image_from_url(df["img_url"][17])
+    image = utils.get_image_from_url("https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/456433/sub/goods_456433_sub20_3x4.jpg?width=600")    
+    image = image.crop((0, 100, 600, 700))
+
+    ColorThemeClassifier.plot_pixel_colors(image, n=5000)
+
+    plt.show()
 
     ColorThemeClassifier.show_data(image)
